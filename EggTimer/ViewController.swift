493edacf37f6bcd5801f3dcbef294f93fdf851ff
Ahmad Ali <ahmad.abdulmuttaleb@gmail.com>
2021-02-05 00:49:18 +0200

    adding functionality and adding timer

--- EggTimer/ViewController.swift
+++ EggTimer/ViewController.swift
@@ -9,7 +9,31 @@
 import UIKit
 
 class ViewController: UIViewController {
+    let eggTimes = ["Soft": 300, "Medium": 480, "Hard":720]
+    var currentTimer: Int = 0
     
-
+    var timer: Timer?
+    
+    override func viewDidLoad() {
+    }
+    
+    @objc func update(){
+        if(currentTimer > 0){
+            print(currentTimer)
+            currentTimer -= 1
+        }
+    }
+    
+    @IBAction func hardnessSelected(_ sender: UIButton) {
+        timer?.invalidate()
+        beginTimer(hardness: sender.currentTitle!)
+    }
+    
+    func beginTimer(hardness: String){
+        let hardnessTime = eggTimes[hardness]
+        currentTimer = hardnessTime ?? 0
+        
+        timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(update), userInfo: nil, repeats: true)
+    }
 
 }
